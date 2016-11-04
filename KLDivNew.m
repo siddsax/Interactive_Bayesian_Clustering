0fe8be6f8453ed6545192ef21e3208e78ed10177
Siddhartha Saxena <ss.siddharthasaxena@gmail.com>
2016-11-04 06:31:10 +0530

    many things done, prior is mostly correct

--- KLDivNew.m
+++ KLDivNew.m
@@ -1,6 +1,5 @@
-function [ sum_KL ] = KLDivNew(P,Q)
+function [ sum_KL ] = KLDivNew(P,Q,K,N)
 sum_KL=0;
-[K,N]=size(q);
 for i = 1:N
   for j = 1:K
       sum_KL = sum_KL + KLDiv(P(j,i),Q(j,i));
--- marg_prob_h.m
+++ marg_prob_h.m
@@ -1,9 +1,9 @@
 function [ P_h ] = marg_prob_h( X, co_var_mat_h, prior_h, mu_h )
-[N,~] = size(X); 
+%[N,~] = size(X); 
 co_var_mat{1} = co_var_mat_h;
-priors(1,1) = prior_h;
+prior(1,1) = prior_h;
 mu(1,:) = mu_h;
-P_h = sum(P_h_givn_x(X, 1, priors, mu, co_var_mat)); %nXk   
+P_h = sum(P_h_givn_x(X, 1, prior, mu, co_var_mat));    
 end
 
 
